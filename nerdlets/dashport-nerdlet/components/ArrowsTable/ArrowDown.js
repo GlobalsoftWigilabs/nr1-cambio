97de484a124af14d0352ee0d3d4f807e5e6ec827
Jeison <yfgaona@ucundinamarca.edu.co>
2021-01-14 12:04:03 -0500

    Arrows issue # 7 UX

--- nerdlets/dashport-nerdlet/components/ArrowsTable/ArrowDown.js
+++ nerdlets/dashport-nerdlet/components/ArrowsTable/ArrowDown.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ArrowDown = ({ color }) => {
     return (
-        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg" >
+        <svg width="0.5vw" height="0.5vw" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg" >
             <path d="M1 1L5 5L9 1" stroke={color} strokeWidth="2"/>
         </svg >
     )
